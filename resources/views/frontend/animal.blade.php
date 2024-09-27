510a4d9e5c9da90d18bb4aade4c2ba242576758a
mashhood rehman <mashhoodr.rehman@gmail.com>
2024-09-27 20:24:16 +0500

    File Downlaod

--- resources/views/frontend/animal.blade.php
+++ resources/views/frontend/animal.blade.php
@@ -103,10 +103,10 @@
                             <div class="inner-box">
                                 <figure class="image-box"><img src="{{asset($relatedAnimal->image)}}" alt=""></figure>
                                 <div class="content-box">
-                                    <h3><a href="{{route('frontend.find.animal' , $relatedAnimal->slug)}}">{{$relatedAnimal->name}}</a></h3>
+                                    <h3><a href="{{route('frontend.find.animal' , $relatedAnimal->slug)}}">{{$relatedAnimal->title}}</a></h3>
                                 </div>
                                 <div class="overlay-content">
-                                    <h3><a href="{{route('frontend.find.animal' , $relatedAnimal->slug)}}">{{$relatedAnimal->name}}</a></h3>
+                                    <h3><a href="{{route('frontend.find.animal' , $relatedAnimal->slug)}}">{{$relatedAnimal->title}}</a></h3>
                                     <!-- <p>There are many type of <br />variations pass not available</p> -->
                                     <div class="link"><a href="{{route('frontend.find.animal' , $relatedAnimal->slug)}}"><i class="flaticon-right-arrow"></i></a></div>
                                 </div>
