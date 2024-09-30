79a681acf74bb284cea8cab61a30ac2128c897a4
Hafiz Adil <hafizadil321@gmail.com>
2024-09-30 15:27:41 +0500

    adil changes

--- resources/views/frontend/gallery/topas.blade.php
+++ resources/views/frontend/gallery/topas.blade.php
@@ -19,14 +19,11 @@
 
     <section class="pt-5">
         <div class="auto-container">
-            <div class="row clearfix">
-                <h1 class="pb-3">Tobas</h1>
-                <p>
-                lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-           
+            <div class="">
+                <h1 class="pb-3">{{$topasGalleryContent->data['title']??''}}</h1>
+                {!! $topasGalleryContent->data['details']??'' !!}
             </div>
-          
+
         </div>
     </section>
 
