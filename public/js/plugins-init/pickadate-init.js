e991708e05e29a48744706379f961649113186cf
Hafiz Adil <hafizadil321@gmail.com>
2024-10-01 12:17:04 +0500

    admin panel qa issues

--- public/js/plugins-init/pickadate-init.js
+++ public/js/plugins-init/pickadate-init.js
@@ -2,6 +2,8 @@
     "use strict"
 
     //date picker classic default
-    $('.datepicker-default').pickadate();
+    $('.datepicker-default').pickadate({
+        min: new Date(),      // Disable past dates by setting the minimum date to today
+    });
 
 })(jQuery);
