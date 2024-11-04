bde4705fd795b89aaba012dd0c57a55d4837c524
mashhood rehman <mashhoodr.rehman@gmail.com>
2024-11-04 18:55:56 +0500

    Compressed Image added

--- resources/views/frontend/animal.blade.php
+++ resources/views/frontend/animal.blade.php
@@ -141,7 +141,7 @@
                             <div class="gallery-block-two">
                                 <div class="inner-box">
                                     <figure class="image-box">
-                                        <img src="${assetPath}/${gallery.thumb}" alt="">
+                                        <img src="${assetPath}/${gallery.compressed}" alt="">
                                     </figure>
                                     <div class="view-box d-flex align-items-center flex-column justify-content-center">
                                         <a href="${assetPath}/${gallery.image}"
