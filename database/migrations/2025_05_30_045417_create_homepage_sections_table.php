cf52fb81b5bdc3a2384fd6777b5c49c0919a8b0b
Ismaeel <ismailsaddiqi07@gmail.com>
2025-05-30 09:57:59 +0500

    pulling

---
+++ database/migrations/2025_05_30_045417_create_homepage_sections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('homepage_sections', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();            // Title displayed in the section
+            $table->text('content')->nullable();            // Optional content or description
+            $table->string('background_image')->nullable(); // Image URL/path for background
+            $table->string('button_text')->nullable();       // Button text
+            $table->string('button_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('homepage_sections');
+    }
+};
