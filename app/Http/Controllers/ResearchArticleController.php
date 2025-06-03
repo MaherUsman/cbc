4a8833867eaf631dc8a63ef792daf4f818e3c2ec
Azhar Raza <walisystems123@gmail.com>
2025-06-03 10:10:48 +0500

    06/03/25 10:10:48 Reserch Articles with gallery CRUD

---
+++ app/Http/Controllers/ResearchArticleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataTables\ResearchArticleDataTable;
+use App\Http\Requests\ResearchArticleStoreRequest;
+use App\Http\Requests\ResearchArticleUpdateRequest;
+use App\Models\ResearchArticle;
+use App\Services\ResearchArticleService;
+use Illuminate\Http\Request;
+
+class ResearchArticleController extends Controller
+{
+    public $researchArticleService;
+
+    public function __construct(ResearchArticleService $researchArticleService)
+    {
+        $this->researchArticleService= $researchArticleService;
+    }
+
+    public function index(ResearchArticleDataTable $dataTable)
+    {
+        return $dataTable->render('admin.researchArticle.index');
+    }
+
+    public function create()
+    {
+        return view('admin.researchArticle.create');
+    }
+
+    public function store(ResearchArticleStoreRequest $request)
+    {
+        return $this->researchArticleService->store($request);
+    }
+
+    public function edit(ResearchArticle $researchArticle)
+    {
+        return view('admin.researchArticle.edit', compact('researchArticle'));
+    }
+
+    public function update(ResearchArticleUpdateRequest $request, ResearchArticle $researchArticle)
+    {
+        return $this->researchArticleService->update($request , $researchArticle);
+    }
+
+    public function destroy(ResearchArticle $researchArticle)
+    {
+        return $this->researchArticleService->destroy($researchArticle);
+    }
+}
---
+++ resources/views/admin/researchArticleGallery/gallery_items.blade.php
@@ -0,0 +1,33 @@
+<!-- resources/views/admin/blogGallery/gallery_items.blade.php -->
+@foreach($researchArticleGalleries as $gallery)
+    <div class="col-lg-3 col-md-6 mb-4">
+        <div class="gallery-img-wrapper position-relative w-100 h-100">
+            <a href="{{asset($gallery->image)}}" data-src="{{asset($gallery->image)}}" class="lg-item">
+                <img src="{{asset($gallery->image)}}" class="rounded" alt="" style="width:100%;">
+            </a>
+            <div class="gallery-overlay rounded">
+                <div class="overlay-icons-wrapper w-100 d-flex flex-column align-items-end">
+                    <div class="overlay-icon mt-2">
+                        <a href="{{ route('researchArticleGalleries.edit', $gallery) }}">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </a>
+                    </div>
+{{--                    <div class="overlay-icon mt-2">--}}
+{{--                        <a href="#" class="editImage" data-id="{{ $gallery->id }}" data-image="{{ asset($gallery->image) }}">--}}
+{{--                            <i class="fa-solid fa-pen-to-square"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+                    <div class="overlay-icon mt-2">
+                        <a href="#" data-url="{{ route('researchArticleGalleries.destroy', $gallery) }}" title="Delete"
+                           class="deleteRecord" data-bs-toggle="tooltip" data-bs-placement="top">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="img-title mt-3">
+                    <p>{{ $gallery->title }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
