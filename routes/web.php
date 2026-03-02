a0023418afdeada9242737f606c19a33ee08b8ca
unknown <mkhurramilyas@gmail.com>
2026-03-02 13:06:10 +0500

    url change for student

--- routes/web.php
+++ routes/web.php
@@ -243,7 +243,7 @@ Route::group(['as' => 'frontend.'], function () {
     Route::get('events', [\App\Http\Controllers\Frontend\EventController::class, 'index'])->name('events.index');
     Route::get('research-article', [\App\Http\Controllers\Frontend\HomeController::class, 'rearchArticle'])->name('rearchArticle');
     Route::get('security', [\App\Http\Controllers\Frontend\HomeController::class, 'security'])->name('security');
-    Route::get('our-students', [\App\Http\Controllers\Frontend\StudentController::class, 'index'])->name('students.index');
+    Route::get('our-vet-dr', [\App\Http\Controllers\Frontend\StudentController::class, 'index'])->name('students.index');
     Route::get('visitors', [\App\Http\Controllers\Frontend\GalleryController::class, 'visitorsGallery'])->name('visitors.gallery');
     Route::get('activities', [\App\Http\Controllers\Frontend\GalleryController::class, 'activitiesGallery'])->name('activities.gallery');
     Route::get('search/animals', [\App\Http\Controllers\Frontend\AnimalController::class, 'searchAnimal'])->name('search.animal');
